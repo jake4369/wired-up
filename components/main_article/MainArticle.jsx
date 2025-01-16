28ee6cae23850e97c795f2c7ca2f8bf8a9ccb181
Jake Samuels <samuels242@googlemail.com>
2025-01-16 23:39:19 +0000

    Main Article - Homepage

---
+++ components/main_article/MainArticle.jsx
@@ -0,0 +1,30 @@
+// NextJS
+import Image from "next/image";
+
+const MainArticle = () => {
+  return (
+    <article className="main-article">
+      <Image
+        src="/assets/images/image-web-3-desktop.jpg"
+        alt="Colored blocks"
+        width={1460}
+        height={1168}
+        priority={true}
+      />
+
+      <div class="main-article__text">
+        <h1>The Bright Future of Web 3.0?</h1>
+
+        <p>
+          We dive into the next evolution of the web that claims to put the
+          power of the platforms back into the hands of the people. But is it
+          really fulfilling its promise?
+        </p>
+
+        <button className="btn__read-more">Read More</button>
+      </div>
+    </article>
+  );
+};
+
+export default MainArticle;
